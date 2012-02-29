6a8139513ed83b4c23f12b7f12853a524e774777
Samuel Hubbard <hsamuel82@gmail.com>
2012-02-29 07:13:54 -0800

    Added SSL in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,4 +67,8 @@ ThePub::Application.configure do
 
 	# Precompile Blueprint CSS
 	config.assets.precompile += [ "blueprint/*.css" ]
+
+	# Force all access to the app over SSL, use Strict-Transport-Security,
+	# and use secure cookies.
+	config.force_ssl = true
 end
